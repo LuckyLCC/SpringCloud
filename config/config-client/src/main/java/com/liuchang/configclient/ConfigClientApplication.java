4ad41e80a3aca2d49735521af3240f2c8e852c91
LuckyLCC <278063497@qq.com>
2018-09-30 11:42:02 +0800

    ww

--- config/config-client/src/main/java/com/liuchang/configclient/ConfigClientApplication.java
+++ config/config-client/src/main/java/com/liuchang/configclient/ConfigClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//liuchang
 @SpringBootApplication
 @RestController
 public class ConfigClientApplication {
--- config/config-server-git/src/main/java/com/liuchang/configservergit/ConfigServerGitApplication.java
+++ config/config-server-git/src/main/java/com/liuchang/configservergit/ConfigServerGitApplication.java
@@ -2,8 +2,10 @@ package com.liuchang.configservergit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+//liuchang
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerGitApplication {
 
     public static void main(String[] args) {
